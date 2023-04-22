6035a5a1637376e02c022e7bd3182bb846cc8d24
Aaron Vegu <aaron@reckoner-rde.io>
2023-04-22 09:16:43 -0600

    Models creation

--- PlatziAwards/polls/models.py
+++ PlatziAwards/polls/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    # columns
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    # string function
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    # columns
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # string function
+    def __str__(self):
+        return self.choice_text
